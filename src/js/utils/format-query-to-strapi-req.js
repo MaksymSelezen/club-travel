3c407377437bd95f344b97a74ab2eac36d2ad2bf
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-05-20 21:12:03 +0300

    filtered strapi request

--- src/js/utils/format-query-to-strapi-req.js
+++ src/js/utils/format-query-to-strapi-req.js
@@ -1,36 +1,44 @@
 import { API_URL } from '../services/api/constants';
 
 export const convertStateToStrapiQuery = (state) => {
-
   const strapiParams = new URLSearchParams();
-  strapiParams.set('populate[country][populate]', '*');
+
+  strapiParams.set('populate[country][fields][0]', 'name');
+  strapiParams.set('populate[country][fields][1]', 'slug');
+
+  strapiParams.set('populate[region][fields][0]', 'name');
+
+  strapiParams.set('populate[gallery][fields][0]', 'url');
+  strapiParams.set('populate[gallery][fields][1]', 'alternativeText');
+
+  strapiParams.set('populate[offers][fields][0]', 'price');
+  strapiParams.set('populate[offers][fields][1]', 'mealType');
+  strapiParams.set('populate[offers][fields][2]', 'tourComposition');
+  strapiParams.set('populate[offers][fields][3]', 'departureCitySlug');
+  strapiParams.set('populate[offers][fields][4]', 'duration');
 
   if(state.direction) {
     strapiParams.set('filters[country][slug][$eq]', state.direction);
   }
-
   strapiParams.set('filters[priceForPerson][$gte]', state.price.min);
   strapiParams.set('filters[priceForPerson][$lte]', state.price.max);
 
   const appendStrapiHotelsArray = (path, arrayData) => {
     arrayData.forEach((value, index) => {
       strapiParams.set(`filters${path}[$in][${index}]`, value);
-    })
+    });
   };
-
-    const appendStrapiOffersArray = (path, arrayData) => {
+  const appendStrapiOffersArray = (field, arrayData) => {
     arrayData.forEach((value, index) => {
-      strapiParams.set(`filters[offers]${path}[$in][${index}]`, value);
-      strapiParams.set(`populate[offers][filters]${path}[$in][${index}]`, value);
-    })
+      strapiParams.set(`filters[offers][${field}][$in][${index}]`, value);
+      strapiParams.set(`populate[offers][filters][${field}][$in][${index}]`, value);
+    });
   };
 
-  // if(state.accomondation.length) appendStrapiHotelsArray('[stars]', state.accomondation);
-  if(state.meal.length) appendStrapiOffersArray('[mealType]', state.meal);
-  if(state.tourComposition.length) appendStrapiOffersArray('[tourComposition]', state.tourComposition);
-  if(state.departureCity.length) appendStrapiOffersArray('[departureCitySlug]', state.departureCity);
+  if(state.meal.length) appendStrapiOffersArray('mealType', state.meal);
+  if(state.tourComposition.length) appendStrapiOffersArray('tourComposition', state.tourComposition);
+  if(state.departureCity.length) appendStrapiOffersArray('departureCitySlug', state.departureCity);
   if(state.regions.length) appendStrapiHotelsArray('[region][slug]', state.regions);
   
   return strapiParams.toString();
 }
-

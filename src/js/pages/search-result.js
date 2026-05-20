a635f5791b03e95ef987d69d2f0ea0405ddbd13e
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-05-20 23:01:24 +0300

    modified search strapi request

--- src/js/pages/search-result.js
+++ src/js/pages/search-result.js
@@ -5,10 +5,15 @@ import '@/js/sections/main/hero-swiper.js';
 import '@/js/utils/restore-filter-from-url.js';
 import '@/js/components/tour-search.js';
 
+
 import { initSelect } from '@/js/ui/custom-select.js';
 import { initCardsSwiper } from '@/js/components/cards-swiper.js';
 import { initAccordion } from '@/js/sections/search-result/init-accordion.js';
 import { initSortInCard } from '@/js/sections/search-result/init-sortIn-card.js';
+import { findHotels } from '../services/api/findHotels.js';
+import { getFilterState } from '../services/api/getQueries.js';
+import { convertStateToStrapiQuery } from '../utils/format-query-to-strapi-req.js';
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   initSelect();
@@ -20,3 +25,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   initAccordion();
   initSortInCard();
 });
+
+const state =  getFilterState();
+const strapiQueryString = convertStateToStrapiQuery(state);
+const hotels = await findHotels(strapiQueryString);
+console.log(hotels);
+

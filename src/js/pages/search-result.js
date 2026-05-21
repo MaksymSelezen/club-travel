ee7ac6d4675d40d771a678559cf094057caba0d3
Vadim-48 <rvadim4835@gmail.com>
2026-05-21 16:50:58 +0300

    fix search-result.js after merge

--- src/js/pages/search-result.js
+++ src/js/pages/search-result.js
@@ -7,19 +7,19 @@ import '@/js/components/tour-search.js';
 
 
 import { initSelect } from '@/js/ui/custom-select.js';
-import { initCardsSwiper } from '@/js/components/cards-swiper.js';
-import { initAccordion } from '@/js/sections/search-result/init-accordion.js';
-import { initSortInCard } from '@/js/sections/search-result/init-sortIn-card.js';
-import { findHotels } from '../services/api/findHotels.js';
-import { getFilterState } from '../services/api/getQueries.js';
-import { convertStateToStrapiQuery } from '../utils/format-query-to-strapi-req.js';
+import { renderHotelCards } from '@/js/sections/search-result/render-hotel-cards.js';
+// import { initCardsSwiper } from '@/js/components/cards-swiper.js';
+// import { initAccordion } from '@/js/sections/search-result/init-accordion.js';
+// import { initSortInCard } from '@/js/sections/search-result/init-sortIn-card.js';
+// import { findHotels } from '../services/api/findHotels.js';
+// import { getFilterState } from '../services/api/getQueries.js';
+// import { convertStateToStrapiQuery } from '../utils/format-query-to-strapi-req.js';
 
 
 document.addEventListener('DOMContentLoaded', async () => {
   initSelect();
 
   await renderHotelCards()
-
   // initSortInCard();
 });
 
